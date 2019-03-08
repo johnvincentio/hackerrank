661cfcbf0d245d551a401d5b3928d572856496dc
johnvincentio <john@johnvincent.io>
2019-03-08 10:42:40 -0500

    add

---
+++ src/template/a.js
@@ -0,0 +1,47 @@
+
+/*
+{Challenge URL}
+
+{Challenge name}
+*/
+
+/* eslint-disable no-plusplus */
+
+const convert = {};
+
+let inputString = '';
+let currentLine = 0;
+
+function readLine() {
+	console.error('inputString[currentLine] ', inputString[currentLine]);
+	return inputString[currentLine++];
+}
+
+function minimumDistances(a) { }	// replace from challenge
+
+convert.main = function main(input) {
+	currentLine = 0;
+	inputString = input;
+
+	/* insert from challenge */
+
+	console.log(`result ${result}\n`);
+	return result;
+}
+
+module.exports = convert;
+
+function main(input) {
+	currentLine = 0;
+	inputString = input;
+
+	/* insert from challenge */
+
+	console.log(`result ${result}\n`);
+	return result;
+}
+
+module.exports = {
+	main,
+	anotherFunction
+};
