ec9110012c23428f402bc83fff236b27bbeffcb7
johnvincentio <john@johnvincent.io>
2021-08-03 18:47:00 -0400

    add

--- src/easy/2/dynamic-array/a.js
+++ src/easy/2/dynamic-array/a.js
@@ -1,7 +1,4 @@
-//
-
 /*
-
 https://www.hackerrank.com/challenges/dynamic-array/problem
 
 dynamic-array
@@ -9,11 +6,40 @@ dynamic-array
 
 /* eslint-disable no-plusplus */
 
+let currentLine = 0;
+let inputString = '';
+
+function readLine() {
+  return inputString[currentLine++];
+}
+
+function dynamicArray(n, queries) {
+  console.log('>>> dynamicArray; n ', n, ' queries length ', queries.length);
+  // Write your code here
+}
+
 function main(input) {
   currentLine = 0;
   inputString = input;
 
-  /* insert from challenge */
+  const firstMultipleInput = readLine().replace(/\s+$/g, '').split(' ');
+
+  const n = parseInt(firstMultipleInput[0], 10);
+  console.log(`n ${n}\n`);
+
+  const q = parseInt(firstMultipleInput[1], 10);
+  console.log(`q ${q}\n`);
+
+  let queries = Array(q);
+
+  for (let i = 0; i < q; i++) {
+    queries[i] = readLine()
+      .replace(/\s+$/g, '')
+      .split(' ')
+      .map((queriesTemp) => parseInt(queriesTemp, 10));
+  }
+
+  const result = dynamicArray(n, queries);
 
   console.log(`result ${result}\n`);
   return result;
@@ -21,5 +47,5 @@ function main(input) {
 
 module.exports = {
   main,
-  anotherFunction,
+  dynamicArray,
 };
