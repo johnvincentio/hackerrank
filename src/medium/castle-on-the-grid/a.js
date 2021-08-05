646e261877ef8403e33abb0953419c7cf5bb2a75
johnvincentio <john@johnvincent.io>
2021-08-04 21:21:00 -0400

    add

--- src/medium/castle-on-the-grid/a.js
+++ src/medium/castle-on-the-grid/a.js
@@ -28,10 +28,16 @@ const XINC = [1, -1, 0, 0];
 const YINC = [0, 0, 1, -1];
 const MAX_MOVE_TYPES = 4;
 
-function printBoard(text, board) {
-	console.log('   Board for ', text)
+let MINIMUM_MOVES = 32000;
+
+function printBoard(text, board, table) {
+	console.log('   Print for ', text);
 	for (let x = 0; x < LIMIT; x++) {
-		console.log('      row ', x, ' board ', board[x].join(''));
+		console.log('   board; row ', x, ' board ', board[x].join(''));
+	}
+	console.log("");
+	for (let x = 0; x < LIMIT*LIMIT; x++) {
+		console.log('   table; row ', x, ' table ', table[x]);
 	}
 }
 
@@ -46,12 +52,13 @@ function start(grid, startX, startY, goalX, goalY) {
 	console.log('>>> start');
 	LIMIT = grid.length;
 	const board = [...Array(LIMIT)].map(j=>Array(LIMIT).fill(EMPTY));
+	const table = Array(LIMIT*LIMIT).fill(NO_MOVE);
 	for (let x = 0; x < LIMIT; x++) {
 		board[x] = grid[x].split('');
 	}
 	board[goalX][goalY] = GOAL;
-	printBoard('Initial', board);
-	nextMove(1, 0, startX, startY, board);
+	printBoard('Initial', board, table);
+	nextMove(1, 0, startX, startY, board, table);
 	console.log('<<< start');
 }
 
@@ -59,37 +66,57 @@ function checkValid(x, y, board, moveType, previousType) {
 	if (x < 0 || x >= LIMIT || y < 0 || y >= LIMIT) return false;
 	if (board[x][y] === GOAL) return true;
 	if (board[x][y] !== EMPTY) return false;
-	if (moveType === 0 && previousType === 1) return false;
-	if (moveType === 1 && previousType === 0) return false;
-	if (moveType === 2 && previousType === 3) return false;
-	if (moveType === 3 && previousType === 2) return false;
+	if (previousType === 0) return true;					// was the initial move
+	if (moveType === previousType) return true;		// can keep moving in the same direction
+	if (previousType === 1 && moveType === 2) return false;
+	if (previousType === 2 && moveType === 1) return false;
+	if (previousType === 3 && moveType === 4) return false;
+	if (previousType === 4 && moveType === 3) return false;
 	return true;
 }
 
-function nextMove(move, previousType, previousX, previousY, board) {
+function countMoves(table) {
+	let lastMove = 0;
+	let total = 0;
+	for (let i = 1; i < table.length; i++) {
+		console.log('i ', i, ' table ', table[i]);
+		if (table[i] === -1) break;
+		if (table[i] !== lastMove) {
+			lastMove = table[i];
+			total++;
+		}
+	}
+	return total;
+}
+
+function nextMove(move, previousType, previousX, previousY, board, table) {
 	// prettier-ignore
 	console.log('>>> NextMove; move ', move, ' previousType ', previousType, ' previousX ', previousX, ' previousY ', previousY);
+	if (board[previousX][previousY] === GOAL) {
+		table[move - 1] = previousType;
+		printBoard('\n\nVICTORY', board, table);
+		const totalMoves = countMoves(table);
+		console.log('totalMoves ', totalMoves);
+		if (totalMoves < MINIMUM_MOVES) MINIMUM_MOVES = totalMoves;
+		table[move - 1] = NO_MOVE;
+
+		return;
+	}
 	board[previousX][previousY] = move;
-	printBoard('NextMove added move', board);
+	table[move - 1] = previousType;
+	printBoard('NextMove added move', board, table);
 
-	if (move > 9) return;
-
-	// if (move >= XYLIMIT)
-	// 	printBoard (board);
 	let newX; let newY;
-	for (let moveType = 0; moveType < MAX_MOVE_TYPES; moveType++) {
-		newX = previousX + XINC[moveType];
-		newY = previousY + YINC[moveType];
+	for (let moveType = 1; moveType <= MAX_MOVE_TYPES; moveType++) {
+		newX = previousX + XINC[moveType - 1];
+		newY = previousY + YINC[moveType - 1];
 		if (checkValid(newX, newY, board, moveType, previousType)) {
-			nextMove(move + 1, moveType, newX, newY, board);
+			nextMove(move + 1, moveType, newX, newY, board, table);
 		}
-		// if (newX < 0 || newX >= LIMIT || newY < 0 || newY >= LIMIT || board[newX][newY] === BLOCK) {
-		// 	;
-		// }
-		// else nextMove(move + 1, moveType, newX, newY, board);
 	}
 	board[previousX][previousY] = EMPTY;
-	printBoard('<<< NextMove; board', board);
+	table[move - 1] = NO_MOVE;
+	printBoard('<<< NextMove; board', board, table);
 }
 
 /*
@@ -108,7 +135,7 @@ function minimumMoves(grid, startX, startY, goalX, goalY) {
 	// prettier-ignore
 	console.log('>>> minimumMoves; grid ', grid, ' startX ', startX, ' startY ', startY, ' goalX ', goalX, ' goalY ', goalY);
 	start(grid, startX, startY, goalX, goalY);
-	return 89;
+	return MINIMUM_MOVES;
 }
 
 convert.main = function main(input) {
